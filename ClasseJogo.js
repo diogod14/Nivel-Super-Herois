82af753de5d6d1b074674b11cc907b8cdd1c4385
diogo <digobonilia@gmail.com>
2023-11-05 17:38:27 -0300

    "Escrevendo as Classes de Um Jogo"

---
+++ ClasseJogo.js
@@ -0,0 +1,35 @@
+class superHeroi{
+     constructor ( nome, idade, tipo){
+        this.nome = nome 
+        this.idade = idade
+        this.tipo = tipo
+     }
+      
+     escreve(){
+        console.log(`o ${this.tipo} atacou usando ${ataque}`)
+     }
+}
+
+
+
+let heroi = new  superHeroi( "homem-aranha", "22" , " guerrero" )
+let ataque = " "
+
+if( this.tipo){ 
+    mago = " usou magia"
+}
+
+else if (this.tipo){
+    guerrero = "usou espada"
+}
+
+else if(this.tipo){
+    monge = " usou arte marcial"
+}
+
+else if(tipo){
+    ninja ="usou shuriken"
+}
+console.log(`o ${this.tipo} atacou usando ${ataque}`)
+ 
+heroi.escreve()
